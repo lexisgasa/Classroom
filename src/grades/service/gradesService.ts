03dde945529c86ee2037e7fcb2abeba38b5bdec7
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-07-02 15:20:08 +0200

    Create getGradesTotal funcion, returns the length in number of the array passed as argument

--- src/grades/service/gradesService.ts
+++ src/grades/service/gradesService.ts
@@ -3,9 +3,9 @@ import { showErrorModal } from "../../dom/index.js";
 import { Grade } from "../../types";
 import { generateId } from "../../utils.js";
 
-// Crea una función para obtener el total de notas
-// La función debe recibir un array de notas y devolver el total de notas
-// export const getGradesTotal =
+export const getGradesTotal = (grades: Grade[]): number => {
+  return grades.length;
+};
 
 // Crea una función para obtener los datos completos de una nota
 // La función debe recibir una nota
