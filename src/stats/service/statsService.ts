35ab877da06a76f86ad4e71591b7da1479f2aa7e
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-07-04 23:29:57 +0200

    Fix passed and failed students, and passed and failed average

--- src/stats/service/statsService.ts
+++ src/stats/service/statsService.ts
@@ -5,31 +5,40 @@ import { CourseStats } from "../../types";
 // La función debe recibir el id de un curso
 // La función debe devolver un objeto de tipo CourseStats
 export const getCourseStats = (courseId: number): CourseStats => {
-  const totalStudents = grades.map((grade) => grade.studentId);
-  const studentsCount = totalStudents.length;
+  const totalCourses = grades.filter((grade) => grade.courseId === courseId);
 
-  const passedStudents = grades.filter((grade) => grade.value >= 5);
+  const studentsInCourse = totalCourses.map((grade) => grade.studentId);
+
+  const studentsCount = studentsInCourse.length;
+
+  const approvalScore = 5;
+  const passedStudents = totalCourses.filter(
+    (grade) => grade.value >= approvalScore
+  );
   const passedStudentsCount = passedStudents.length;
 
   const passedStudentsPercentage = (passedStudentsCount / studentsCount) * 100;
 
-  const failedStudents = grades.filter((grade) => grade.value < 5);
+  const failedStudents = totalCourses.filter(
+    (grade) => grade.value < approvalScore
+  );
   const failedStudentsCount = failedStudents.length;
 
   const failedCountPercentage = (failedStudentsCount / studentsCount) * 100;
 
-  const totalGrades = grades.map((grade) => grade.value);
+  const totalGrades = totalCourses.map((grade) => grade.value);
   const gradesSum = totalGrades.reduce(
     (accumulator, currentValue) => accumulator + currentValue
   );
   const gradesCount = totalGrades.length;
   const averageGrade = gradesSum / gradesCount;
 
-  const sortedGrades = totalGrades.toSorted();
-  const highestGrade = sortedGrades.length > 0 ? sortedGrades.at(-1)! : 0;
-  const highestGradeStudent = grades.find(
-    (grade) => grade.value === highestGrade
+  const sortedGrades = totalGrades.toSorted((a, b) => a - b);
+  const highestGrade = sortedGrades[sortedGrades.length - 1];
+  const highestGradeStudent = totalCourses.find(
+    (student) => student.value === highestGrade
   );
+  totalCourses;
   const highestGradeStudentId = highestGradeStudent
     ? highestGradeStudent.studentId
     : 0;
